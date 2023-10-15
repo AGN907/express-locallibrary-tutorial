cb15fd662a88889d05222b1b5b1392c151d56e36
AGN <itzme907@gmail.com>
2023-10-16 01:42:02 +0300

    First version of application moved into Github

---
+++ models/author.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose')
+const { DateTime } = require('luxon')
+
+const Schema = mongoose.Schema;
+
+const AuthorSchema = new Schema({
+  first_name: { type: String, required: true, maxLength: 100 },
+  family_name: { type: String, required: true, maxLength: 100 },
+  date_of_birth: { type: Date },
+  date_of_death: { type: Date }
+})
+
+AuthorSchema.virtual('name').get(function() {
+  let fullname = ''
+  if (this.first_name && this.family_name) {
+    fullname = `${this.family_name}, ${this.first_name}`
+  }
+  return fullname
+})
+
+AuthorSchema.virtual('lifespan').get(function() {
+  const birth = this.date_of_birth ? DateTime.fromJSDate(this.date_of_birth)
+    .toLocaleString(DateTime.DATE_MED) : ''
+
+  const death = this.date_of_death ? DateTime.fromJSDate(this.date_of_death)
+    .toLocaleString(DateTime.DATE_MED) : ''
+
+  return `${birth} - ${death}`
+})
+
+AuthorSchema.virtual('date_of_birth_yyyy_mm_dd').get(function() {
+  return DateTime.fromJSDate(this.date_of_birth).toISODate()
+
+})
+
+AuthorSchema.virtual('date_of_death_yyyy_mm_dd').get(function() {
+  return DateTime.fromJSDate(this.date_of_death).toISODate()
+
+})
+AuthorSchema.virtual('url').get(function() {
+  return `/catalog/author/${this._id}`
+})
+
+
+module.exports = mongoose.model('Author', AuthorSchema)
